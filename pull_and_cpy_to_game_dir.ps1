dc15a29f27b48c9f215124557d7f15b20d37d1df
Niukron <70972633+Niukron@users.noreply.github.com>
2020-11-18 23:29:39 +0100

    Update

--- pull_and_cpy_to_game_dir.ps1
+++ pull_and_cpy_to_game_dir.ps1
@@ -5,7 +5,7 @@
 $MAIN_GAME_FOLDER = 'A:\Gry\SteamLibrary\steamapps\common\Crusader Kings III\'
 
 # DEBUG MODE - nie pobiera danych z transifexa oraz nie kopiuje plików do folderu z grą.
-$DEBUG_MODE = 0
+$DEBUG_MODE = 1
 # Jeśli chcesz widzieć jakie pliki są kopiowane - włącz tą opcję
 $LOG_CPY_FILES = 0
 # Jeśli chcesz użyć nowszego cli transifexa i pominąć stare skrypty zmień na 1
@@ -17,54 +17,66 @@ $str_article_to_replace = '_article:0 ""'
 $TRANSIFEX_COMPILED_FILES = "$PSScriptRoot\ck3transifex\Spolszczenie_CK3\*"
 $CK3_PL_ADDONS = "$PSScriptRoot\ck3_main\*"
 
+$version_on_github = Invoke-RestMethod https://raw.githubusercontent.com/Niukron/CK3PLCustomAddons/main/version
+$version_local = Get-Content -Path "$PSScriptRoot\version"
 
-if(Test-Path $MAIN_GAME_FOLDER) {
+if($version_on_github -ne $version_local) {
+	Write-Host "Na githubie znajdują się nowsze pliki. Pobierz ponownie pliki, aby posiadać aktualne zmiany w custom_loc i skryptach." -ForegroundColor red -BackgroundColor white
+	Write-Host "Kliknij enter, jeśli mimo to chcesz kontynuować. Zamknij okno jeśli chcesz przerwać wykonywanie skryptu..." -ForegroundColor red -BackgroundColor white
+	Read-Host -Prompt "Kontynuować?"
+}
 
-	Write-Host "Pobieranie danych z transifex...
-Oczekiwanie na zakończenie procesu w oknie CMD.."
-	if(!$DEBUG_MODE) { 
-		Start-Process "$PSScriptRoot\ck3transifex\pull_translations_from_transifex.bat" -WorkingDirectory "$PSScriptRoot\ck3transifex\"  -Wait 
-	}
-	Write-Host "Proces zakończony..."
 
-	Get-ChildItem $TRANSIFEX_COMPILED_FILES -Recurse -Filter "*.yml" | ForEach-Object { 
-		(Get-Content $_.FullName) -replace $str_article_to_find_2, $str_article_to_replace  | Set-Content -Encoding UTF8 $_.FullName
-		Write-Host "Usuwanie przedimka 'the' z pliku: " $_.Name
-	}
+	if(Test-Path $MAIN_GAME_FOLDER) {
 
-	Write-Host "Proces zakończony...
-Kopiowanie plików spolszczenia do folderu z grą..."
+		Write-Host "Pobieranie danych z transifex...
+	Oczekiwanie na zakończenie procesu w oknie CMD.."
+		if(!$DEBUG_MODE) { 
+			Start-Process "$PSScriptRoot\ck3transifex\pull_translations_from_transifex.bat" -WorkingDirectory "$PSScriptRoot\ck3transifex\"  -Wait 
+		}
+		Write-Host "Proces zakończony..."
 
-	if($LOG_FILES -And !$DEBUG_MODE)
-	{
-		Copy-item -Force -Recurse -Verbose $TRANSIFEX_COMPILED_FILES -Destination $MAIN_GAME_FOLDER
+		Get-ChildItem $TRANSIFEX_COMPILED_FILES -Recurse -Filter "*.yml" | ForEach-Object { 
+			(Get-Content $_.FullName) -replace $str_article_to_find_2, $str_article_to_replace  | Set-Content -Encoding UTF8 $_.FullName
+			Write-Host "Usuwanie przedimka 'the' z pliku: " $_.Name
+		}
+
 		Write-Host "Proces zakończony...
-Kopiowanie dodatków do spolszczenia..."
-		Copy-item -Force -Recurse -Verbose $CK3_PL_ADDONS -Destination $MAIN_GAME_FOLDER
-		Write-Host "Proces zakończony...
+	Kopiowanie plików spolszczenia do folderu z grą..."
 
-Ukończono instalację plików"
-	} elseif(!$DEBUG_MODE) {
-		Copy-item -Force -Recurse $TRANSIFEX_COMPILED_FILES -Destination $MAIN_GAME_FOLDER
-		Write-Host "Proces zakończony...
-Kopiowanie dodatków do spolszczenia..."
-		Copy-item -Force -Recurse $CK3_PL_ADDONS -Destination $MAIN_GAME_FOLDER
-		Write-Host "Proces zakończony...
+		if($LOG_FILES -And !$DEBUG_MODE)
+		{
+			Copy-item -Force -Recurse -Verbose $TRANSIFEX_COMPILED_FILES -Destination $MAIN_GAME_FOLDER
+			Write-Host "Proces zakończony...
+	Kopiowanie dodatków do spolszczenia..."
+			Copy-item -Force -Recurse -Verbose $CK3_PL_ADDONS -Destination $MAIN_GAME_FOLDER
+			Write-Host "Proces zakończony...
 
-Ukończono instalację plików"
+	Ukończono instalację plików"
+		} elseif(!$DEBUG_MODE) {
+			Copy-item -Force -Recurse $TRANSIFEX_COMPILED_FILES -Destination $MAIN_GAME_FOLDER
+			Write-Host "Proces zakończony...
+	Kopiowanie dodatków do spolszczenia..."
+			Copy-item -Force -Recurse $CK3_PL_ADDONS -Destination $MAIN_GAME_FOLDER
+			Write-Host "Proces zakończony...
+
+	Ukończono instalację plików"
+		
+		}
+		Read-Host -Prompt "Kliknij enter, aby wyjść"
+	} else {
+
+
+	Write-Host "Podany folder z grą nie istnieje.
+
+	Podana ścieżka: $MAIN_GAME_FOLDER 
+
+	Proszę podać poprawny folder w skrypcie
+
+	"
+	Read-Host -Prompt "Kliknij enter, aby wyjść"
 	
 	}
 
-} else {
+	
 
-
-Write-Host "Podany folder z grą nie istnieje.
-
-Podana ścieżka: $MAIN_GAME_FOLDER 
-
-Proszę podać poprawny folder w skrypcie
-
-"
-}
-
-Read-Host -Prompt "Kliknij enter, aby wyjść"
