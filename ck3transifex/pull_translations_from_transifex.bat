f4a85de18b609710cc6dc916f9b1595d808e930d
DrSzuriad <darek.kluczewski@gmail.com>
2022-04-06 21:11:17 +0200

    Update transifex CLI to 3.0

--- ck3transifex/pull_translations_from_transifex.bat
+++ ck3transifex/pull_translations_from_transifex.bat
@@ -1,6 +1,6 @@
 echo "Pulling localisation files from Transifex"
 :: Force pull all translations
-tx.exe pull --all --force --parallel & transifex_to_ck3_files.bat
+tx.exe pull --all --force -translations & transifex_to_ck3_files.bat
 
 
 echo Transifex ukończony.. 
